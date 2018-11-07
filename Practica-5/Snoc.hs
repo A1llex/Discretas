351c35b0d13bea7ec9a081e697010ab2c1bb9e58
Alex Fernandez <41647351+A1llex@users.noreply.github.com>
2018-11-07 10:38:50 -0600

    Snoc Terminada

--- Practica-5/Snoc.hs
+++ Practica-5/Snoc.hs
@@ -7,24 +7,34 @@ module Snoc where
 
  --Ejercicio1.1
  addSnoc :: SnocList a -> a -> SnocList a
- addSnoc = error "Falta Implementar"
+ --solo hay que tomar la snoc list y agregarle un elemento de la forma en la que esta diseñada snolist
+ addSnoc a e = (Snoc (a) e)
 
  --Ejercicio1.2
  ultimo :: SnocList a -> a
- ultimo = error "Falta Implementar"
+ --solo hay que regresar la cabeza
+ --solo com duda no se que debaria pasar si solo contiene Mt
+ ultimo (Snoc (s) e) = e
 
  --Ejercicio1.3
  resto :: SnocList a -> SnocList a
- resto = error "Falta Implementar"
+ --solo hay que quitarle la cabeza de la lista
+ resto (Snoc (s) e) = s
 
  --Ejercicio1.4
  cabeza :: SnocList a -> a
- cabeza = error "Falta Implementar"
+ --solo hay que regresar el ultimo elemento de la cola de la lista
+ cabeza (Snoc (Mt) a) = a
+ cabeza (Snoc (s) a) = cabeza (s)
 
  --Ejercicio1.5
  cola :: SnocList a -> SnocList a
- cola = error "Falta Implementar"
+ -- regresa la lista sin el ultimo lemento de la cola
+ cola (Snoc (Mt) a) = Mt
+ cola (Snoc (s) a) = (Snoc (cola (s)) a)
 
  --Ejercicio1.6
  longitud :: SnocList a -> Int
- longitud = error "Falta Implementar"
+ --solo hay que ir sumando elemento por elemento en la recursion quitandole la cabeza cada vez hasta llegar al caso base
+ longitud (Snoc (Mt) a) = 1
+ longitud (Snoc (s) a) = 1+(longitud (s))
